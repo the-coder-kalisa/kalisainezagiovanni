e1814ff533937756870d3c5e43ebadd5a13d5671
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-03-09 09:36:20 +0200

    hello

--- src/pages/Verification.js
+++ src/pages/Verification.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react'
+import React, { useState} from 'react'
 // import Context from './store.js'
 import Bottomer from './Bottomer'
 import SlackLogo from '../images/SlackLogo'
